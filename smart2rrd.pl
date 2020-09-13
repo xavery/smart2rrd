261aa94632f1df5a6780ab1138e278ebe3c86138
Daniel Kamil Kozar <dkk089@gmail.com>
2020-09-13 23:29:21 +0200

    Add some attributes present on Phison-based SSDs

--- smart2rrd.pl
+++ smart2rrd.pl
@@ -36,9 +36,11 @@ Readonly::Hash my %wanted_attrs => (
   188 => 'Command_Timeout',
   189 => 'High_Fly_Writes',
   190 => 'Airflow_Temperature_Cel',
+  173 => 'MaxAvgErase_Ct',
   177 => 'Wear_Leveling_Count',
   181 => 'Program_Fail_Cnt_Total',
   182 => 'Erase_Fail_Count_Total',
+  231 => 'SSD_Life_Left',
   232 => 'Available_Reservd_Space',
   250 => 'Read_Error_Retry_Rate'
 );
